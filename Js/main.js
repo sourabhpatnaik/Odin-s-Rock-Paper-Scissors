339950a76b26d93b3bac67101d947a2cbc70badc
sourabhpatnaik <sourabhpatnayak19@gmail.com>
2025-01-10 01:46:19 +0530

    Bug Fixes

--- Js/main.js
+++ Js/main.js
@@ -75,7 +75,7 @@ function playRound(humanChoice, computerChoice) {
 //Adding the game rule sets to five round
 
 function playGame() {
-  for (let i = 0; i <= 5; i++) {
+  for (let i = 1; i <= 5; i++) {
     const humanSelection = getHumanChoice();
     const computerSelection = getComputerChoice();
     playRound(humanSelection, computerSelection);
