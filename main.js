1d523c2623f0e4a204a79c50c565d3a563c14c7a
sourabhpatnaik <sourabhpatnayak19@gmail.com>
2025-01-10 01:16:50 +0530

    added the functionality for rock paper scissors game

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+// 0 = Rock
+// 1 = Paper
+// 2 = Scissors
+
+// 0 beats 2
+// 1 beats 0
+// 2 beats 1
+
+
+
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+
 // Function for Getting Computer's Choice
 
 function getComputerChoice(){
@@ -5,25 +21,71 @@ function getComputerChoice(){
     const selectNum = Math.floor(Math.random()*num);
 
     if (selectNum == 0){
-        return "Rock";
+        return "rock";
+        
     }
     else if(selectNum== 1){
-        return "Paper";
+        return "paper";
     }
     else if (selectNum==2){
-        return "Scissors";
+        return "scissors";
     }
 };
 
-console.log(getComputerChoice());
+// console.log(getComputerChoice());
 
 // Function for Getting Human Player's Choice
 
 function getHumanChoice(){
-    let humChoice = prompt("Enter Your Choice : ");
+    let humChoice = prompt("Select rock or paper or scissors : ").toLocaleLowerCase();
 
     return humChoice;
 }
 
-console.log(getHumanChoice());
+// console.log(getHumanChoice());
 
+
+// Function for winner 
+
+function winner(humanChoice , computerChoice){
+   
+    if(humanChoice == computerChoice){
+        return 'tie'
+    }
+    if((humanChoice == "rock" && computerChoice == "scissors") || (humanChoice== "paper" && computerChoice == "rock") || (humanChoice= "scissors" && computerChoice == "paper")){
+        return 'human'
+    }
+
+    else{
+        
+        return 'computer'
+    }
+}
+
+
+// function for play round
+function playRound(humanChoice , computerChoice){
+    let roundWinner = winner(humanChoice , computerChoice);
+
+    if(roundWinner =='tie'){
+        alert("Its a TIE")
+    }
+    else if(roundWinner == 'human'){
+        alert("You Won!!! " + humanChoice +" " + "Beats" +" "+ computerChoice);
+        humanScore++;
+    }
+    else{
+        alert("Computer Wins!!! " + computerChoice +" "+  "Beats"+ " " + humanChoice);
+    }
+}
+
+
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(humanSelection);
+console.log(computerSelection);
+
+
+console.log(playRound(humanSelection,computerSelection));
